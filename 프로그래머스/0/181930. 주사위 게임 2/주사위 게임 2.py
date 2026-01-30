28dfc0e2713f8ecce231e9898435eeccda87c37e
Sua Kang <88168765+sua00@users.noreply.github.com>
2026-01-30 11:31:46 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.01 ms, Memory: 9.24 MB -BaekjoonHub

---
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.py
@@ -0,0 +1,10 @@
+def solution(a, b, c):
+    answer = 0
+    
+    if (a==b)and (b==c) and (c==a):
+        answer = (a+b+c)*(a*a+b*b+c*c)*(a*a*a+b*b*b+c*c*c)
+    elif (a != b) and (b!= c) and (c!= a):
+        answer = (a+b+c)
+    else :
+        answer =(a+b+c) * (a*a+b*b+c*c)
+    return answer
