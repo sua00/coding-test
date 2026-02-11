ff5058bbd4bf10cf892cca300883f4ccac18bde9
Sua Kang <88168765+sua00@users.noreply.github.com>
2026-02-12 04:59:24 +0900

    [Silver V] Title: 숫자 카드, Time: 524 ms, Memory: 118308 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.py
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -1,27 +1,15 @@
-# 첫 번째 배열
 n = int(input())
-arr1 = list(map(int, input().split()))
+cards = set(map(int, input().split()))
 
-# 두 번째 배열
 m = int(input())
-arr2 = list(map(int, input().split()))
+targets = list(map(int, input().split()))
 
-arr1.sort()
+results = []
 
-def binary_search(array, target, start, end):
-    while start <= end:
-        mid = (start+end)//2
-        if array[mid] == target:
-            return 1
-        elif array[mid] > target:
-            end = mid-1
-        else :
-            start = mid+1
-    return 0
+for target in targets:
+    if target in cards:
+        results.append(1)
+    else:
+        results.append(0)
 
-result = []
-
-for i in arr2:
-    result.append(binary_search(arr1, i, 0, n-1))
-
-print(*result)
+print(*results)
