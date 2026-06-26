b9a7db73ea290266116b259e8b4a77aa9dffacfd
Sua Kang <88168765+sua00@users.noreply.github.com>
2026-06-26 11:27:08 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 5.46 ms, Memory: 11.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -0,0 +1,31 @@
+#출발점에서 목적지까지 가장 빨리 가는 길 찾기 : bfs
+#가까운 곳 부터 살펴보다가 운좋게 거기 도착점이면 끝! 그렇기 때문에 bfs
+from collections import deque
+
+def solution(maps):
+    queue = deque()
+    queue.append((0,0))
+    n = len(maps)
+    m = len(maps[0])
+    maps[0][0]= 1 #시작점 표시
+    
+    #방향 정의
+    directions = [(-1,0), (1,0), (0,-1), (0,1)]
+    #queue가 빌 때까지 반복
+    while queue:
+        row, col = queue.popleft()
+        
+        for dr, dc in directions:
+            new_row = row+dr
+            new_col = col+dc
+            
+            #확인1 : map을 벗어나지 않았는가
+            if (0<= new_row < n) and (0<=new_col< m):
+                # 확인 2: 갈 수 있는 길인가
+                if maps[new_row][new_col] == 1:
+                    maps[new_row][new_col] = maps[row][col] +1
+                    queue.append((new_row, new_col))
+    if maps[n-1][m-1] != 1:
+        return maps[n-1][m-1]
+    else:
+        return -1
