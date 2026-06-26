2515ca95e6388a5657584b32f4d1de35155af550
Sua Kang <88168765+sua00@users.noreply.github.com>
2026-06-26 12:56:35 +0900

    [level 3] Title: 네트워크, Time: 0.29 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,18 +1,20 @@
 # dfs/bfs 중 어느 것을 사용해도 괜찮은 문제
 # 단 방문체크가 중요함
 def solution(n, computers):
-    visited= [False] * n #0,1,2번 컴퓨터에 대해서 방문체크
+    #컴퓨터 방문했는지 확인
+    visited = [False] * n
     answer = 0
     
     def dfs(node):
-        visited[node] = True #방문 체크
-        for next_node in range(n): #0부터 n-1컴퓨터까지 모두 확인
-            if computers[node][next_node] ==1 and not visited[next_node]:
-                dfs(next_node)
-                
-    for i in range(n): #0부터 n-1번 컴퓨터 확인
-        if not visited[i]: #아직 방문 안된 컴퓨터 -> 새로운 네트워크
+        visited[node] = True
+        
+        for next_com in range(n):
+            if computers[node][next_com] == 1 and not visited[next_com]:
+                dfs(next_com)
+    
+    for i in range(n):
+        if not visited[i]:
             answer+=1
             dfs(i)
-            
+    
     return answer
