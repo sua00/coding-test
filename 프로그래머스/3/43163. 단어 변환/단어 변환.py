e5566a158cf9b03444d4e60936d2bfd5902a44f2
Sua Kang <88168765+sua00@users.noreply.github.com>
2026-06-27 01:21:50 +0900

    [level 3] Title: 단어 변환, Time: 0.15 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -2,26 +2,24 @@
 from collections import deque
 
 def solution(begin, target, words):
-    visited = set() #단어니까 set으로 만드는게 편함
-    visited.add(begin)
-    
+    visited = [False]*len(words)
+    answer =0
     queue = deque()
-    queue.append((begin,0)) #(현재단어, 변환횟수)
-    
+    queue.append((begin, 0))
     while queue:
-        current_word, count = queue.popleft()
-    
-        if current_word == target:
-            return count
-        
-        for word in words:
-            if word not in visited:
+        start, cnt = queue.popleft()
+        if start == target:
+            return cnt
+        for i in range(len(words)):
+            if not visited[i]:
                 diff = 0
-                for i in range(len(word)):
-                    if current_word[i] != word[i]:
-                        diff+=1
+                for j in range(len(target)):
+                    if start[j] != words[i][j]:
+                        diff+= 1
                 if diff ==1:
-                    visited.add(word)
-                    queue.append((word, count+1))
-
+                    visited[i] =True
+                    queue.append((words[i], cnt+1))
     return 0
+            
+            
+        
