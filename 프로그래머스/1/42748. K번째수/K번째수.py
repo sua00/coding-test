ae680644b223a84d0cce319346296fb8ede28259
Sua Kang <88168765+sua00@users.noreply.github.com>
2026-07-23 23:23:43 +0900

    [level 1] Title: K번째수, Time: 0.02 ms, Memory: 11.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -0,0 +1,10 @@
+def solution(array, commands):
+    answer = []
+    for command in commands :
+        i, j, k = command[0], command[1], command[2]
+        
+        temp = array[i-1:j]
+        temp = sorted(temp)
+        print (temp)
+        answer.append(temp[k-1])
+    return answer
