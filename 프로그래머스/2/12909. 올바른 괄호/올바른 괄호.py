0fcef2a647ca5d0b61641cad1b38121f8236a810
Sua Kang <88168765+sua00@users.noreply.github.com>
2026-08-05 01:34:07 +0900

    [level 2] Title: 올바른 괄호, Time: 3.70 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,14 +1,15 @@
 def solution(s):
     answer = True
-    check = []
+    temp = []
+    #열린 괄호나 닫힌 괄호 하나만 넣고 닫는걸 만날 때마다 가장 최신 열린 괄호부터 닫아준다 -> lifo
     
-    if s[-1] == '(' or s[0]==')':
+    if s[-1] =='(' or s[0] == ')':
         return False
-    for c in s:
+    
+    for c in s :
         if c == '(':
-            check.append(c)
+            temp.append(c)
         else:
-            if not check:
-                return False
-            check.pop()
-    return not check
+            if temp:
+                temp.pop()
+    return not temp
