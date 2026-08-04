eb7e39ff4358aed10f4465e6b1c9c5cabf90836b
Sua Kang <88168765+sua00@users.noreply.github.com>
2026-08-04 21:09:25 +0900

    [level 2] Title: 올바른 괄호, Time: 3.67 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,27 +1,14 @@
 def solution(s):
-    check = []
-    open = "("
     answer = True
-        
-    for i in s:
-        # if (s[0] != open) or (s[-1]== open):
-        #     answer = False
-        #     break
-        if i == open:
-            check.append(i)
+    check = []
+    
+    if s[-1] == '(' or s[0]==')':
+        return False
+    for c in s:
+        if c == '(':
+            check.append(c)
         else:
             if not check:
-                answer = False
-            else:
-                check.pop()
-    if check:
-        answer = False
-        
-    return answer
-
-    
-                
-            
-
-
-        
+                return False
+            check.pop()
+    return not check
