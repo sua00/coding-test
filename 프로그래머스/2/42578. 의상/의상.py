7c7437744161f94ceb79ed57aefa8bf3b0a671be
Sua Kang <88168765+sua00@users.noreply.github.com>
2026-08-05 22:54:37 +0900

    [level 2] Title: 의상, Time: 0.01 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,31 +1,11 @@
 def solution(clothes):
-    #print(clothes)
     answer = 1
     closet = {}
-    for c in clothes:
-        closet[c[1]] = closet.get(c[1],())+(c[0],)
+    for a,b in clothes:
+        closet[b] = closet.get(b,0)+1
     
     for i in closet:
-        answer *= (len(closet[i])+1)
+        answer *= (closet[i]+1)
+    answer = answer -1
         
-    answer -=1
     return answer
-#     if len(closet)>1:
-#         for i in closet:
-#             answer += len(closet[i]) #의상 종류 하나씩만
-#             temp *= len(closet[i]) #모든 의상 종류 다 쓸 때
-            
-#         answer += temp
-#     else:
-#         for i in closet:
-#             answer += len(closet[i]) #의상 종류 하나일 때
-#     #print(answer)
-#     return answer
-# #     print(temp)
-# #     print(answer)
-    
-# #     for i in closet:
-# #         print(i)
-# #         print(len(closet[i]))
-    
-    
