3420a98cd74c7a6e09760a279798bee2d608c3c8
Sua Kang <88168765+sua00@users.noreply.github.com>
2026-08-06 00:40:04 +0900

    [level 2] Title: 가장 큰 수, Time: 36.36 ms, Memory: 22.1 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,14 +1,11 @@
+import string
 def solution(numbers):
     answer = ''
-    #numbers를 str으로 바꿔줘야함
-    numbers = list(map(str, numbers))
     
-    # 정렬 순서를 정하기 위해 문자열을 3번씩 반복하고 비교 : 충분히 늘려서 확인한다
-    numbers.sort(key = lambda x: x*3, reverse = True)
+    numbers = sorted(numbers, key = lambda x: str(x)*3, reverse = True)
     
-    if numbers[0]=='0':
+    for num in numbers:
+        answer+= str(num)
+    if answer[0] == '0':
         return '0'
-    for number in numbers:
-        answer += number
-    
     return answer
