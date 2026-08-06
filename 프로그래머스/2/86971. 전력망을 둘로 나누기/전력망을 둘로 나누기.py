ba24b9eb708ddc4d9575551829801dd76fd31253
Sua Kang <88168765+sua00@users.noreply.github.com>
2026-08-07 01:31:44 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 1.50 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -2,27 +2,27 @@ def solution(n, wires):
     answer = n
     
     def dfs(node):
-        visited[node]= True
-        count = 1
+        visited[node] = True
+        count =1
         
         for next_node in graph[node]:
             if not visited[next_node]:
                 count += dfs(next_node)
         return count
-    
+
     for cut_a, cut_b in wires:
-        
         graph = [[] for _ in range(n+1)]
         
-        for a,b in wires:
-            if a ==cut_a and b==cut_b:
+        for a, b in wires:
+            if a==cut_a and b==cut_b:
                 continue
             
             graph[a].append(b)
             graph[b].append(a)
             
-        visited = [False]*(n+1)
+        visited= [False]*(n+1)
         count = dfs(1)
+        answer = min(answer, abs(count-(n-count)))
         
-        answer =min(answer, abs(count-(n-count)))
+    
     return answer
