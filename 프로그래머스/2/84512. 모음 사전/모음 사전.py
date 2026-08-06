ad8690aba8fd524876ff8dad81d87f4bf3abce07
Sua Kang <88168765+sua00@users.noreply.github.com>
2026-08-07 01:47:16 +0900

    [level 2] Title: 모음 사전, Time: 2.09 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -1,21 +1,26 @@
 def solution(word):
     answer = 0
-    count = 0
+    count= 0
+    
     vowels = ['A','E','I','O','U']
     
+    
     def dfs(current):
-        nonlocal answer, count
-        
-        if len(current)>5:
-            return
-        
-        if current:
-            count +=1
-            
-            if current == word:
-                answer = count
-                return
-        for v in vowels:
-            dfs(current+v)
+            nonlocal answer, count
+
+            if len(current)>5:
+                return 
+
+            if current:
+                count+=1
+
+                if current == word:
+                    answer= count
+                    return
+
+            for v in vowels:
+                dfs(current+v)
+
     dfs("")
+    
     return answer
