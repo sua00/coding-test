cacb90f0c1382c8e7f4b1cfceeb6e9a149aeb52e
Sua Kang <88168765+sua00@users.noreply.github.com>
2026-08-07 00:52:23 +0900

    [level 2] Title: 소수 찾기, Time: 3.14 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -1,32 +1,24 @@
+from itertools import permutations
+
+def is_prime(n):
+    if n <2:
+        return False
+    
+    for i in range(2, int(n**0.5)+1):
+        if n%i ==0:
+            return False
+    return True
+
 def solution(numbers):
+    nums = set()
+    
+    for i in range(1, len(numbers)+1):
+        for p in permutations(numbers, i):
+            nums.add(int("".join(p)))
     answer = 0
     
-    candidates = set() #중복 자동 제거 위함
-    visited = [False] * len(numbers)
-    
-    def dfs(current_num):
-        if current_num != "":
-            candidates.add(int(current_num))
-        
-        for i in range(len(numbers)):
-            if not visited[i] :
-                visited [i] = True
-                dfs(current_num+numbers[i])
-                visited[i]= False
-        
-    dfs("")
-    
-    def is_prime(n):
-        if n < 2: 
-            return False
-        for i in range(2,int(n**0.5)+1):
-            if n%i == 0:
-                return False
-        return True
-    
-    answer= 0
-    for candidate in candidates:
-        if is_prime(candidate):
+    for num in nums:
+        if is_prime(num):
             answer+=1
-        
+            
     return answer
