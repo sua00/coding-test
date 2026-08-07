5a1b99492d938eb56caca51695531eb45a4b90b3
Sua Kang <88168765+sua00@users.noreply.github.com>
2026-08-07 23:14:49 +0900

    [level 2] Title: 큰 수 만들기, Time: 38.25 ms, Memory: 14.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -0,0 +1,15 @@
+def solution(number, k):
+    answer = ''
+    stack = []
+    
+    for num in number:
+        while stack and k>0 and stack[-1] < num:
+            stack.pop()
+            k -=1
+        stack.append(num)
+    
+    if k > 0:
+        stack = stack[:-k]
+    
+    answer = "".join(stack)
+    return answer
