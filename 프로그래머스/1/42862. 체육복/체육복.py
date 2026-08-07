a1d6b01d8700f9201f74459c9b012bf75f47aa59
Sua Kang <88168765+sua00@users.noreply.github.com>
2026-08-08 00:27:50 +0900

    [level 1] Title: 체육복, Time: 0.03 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,14 +1,18 @@
 def solution(n, lost, reserve):
     answer = 0
-    #여벌 체육복을 가져온 학생이 체육복을 도난당했을 수 있습니다. 이때 이 학생은 체육복을 하나만 도난당했다고 가정하며, 남은 체육복이 하나이기에 다른 학생에게는 체육복을 빌려줄 수 없습니다. -> 이런 제약 사항 먼저 처리
-    #[a,b][a,b,c] : a는 여벌 없는 거임
-    common = set(lost)&set(reserve)
-    lost = sorted(set(lost) - common)
-    reserve = sorted(set(reserve) - common)
+    lost = set(lost)
+    reserve = set(reserve)
+    #여벌 체육복을 가져온 학생이 체육복을 도난당했을 수 있습니다. 이때 이 학생은 체육복을 하나만 도난당했다고 가정하며, 남은 체육복이 하나이기에 다른 학생에게는 체육복을 빌려줄 수 없습니다.
+    common = lost & reserve
+    lost = set(lost) -common
+    reserve = set(reserve)-common
+    
+    #체육복 빌려주기
     for r in reserve:
-        if (r-1) in lost:
+        if r-1 in lost:
             lost.remove(r-1)
-        elif (r+1) in lost:
+        elif r+1 in lost:
             lost.remove(r+1)
-    answer = n-len(lost)
+    
+    answer= n-len(lost)
     return answer
