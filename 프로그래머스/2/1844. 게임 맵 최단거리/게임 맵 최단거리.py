e59b9608cf6533127f0d5723f6d28fa2e6cb4982
Sua Kang <88168765+sua00@users.noreply.github.com>
2026-09-01 16:14:16 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 6.63 ms, Memory: 11.1 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,26 +1,38 @@
-#출발점에서 목적지까지 가장 빨리 가는 길 찾기 : bfs
-#가까운 곳 부터 살펴보다가 운좋게 거기 도착점이면 끝! 그렇기 때문에 bfs
+#가중치가 없는 그래프에서 최단거리를 구할 때는 bfs가 대표적
+#bfs는 파이썬에서 queue로 구현
+
 from collections import deque
 
 def solution(maps):
+    answer = 0
+    
+    #map 모양 설정
+    m = len(maps[0]) # m : 열(가로 좌표)
+    n = len(maps) #n: 행(세로 좌표)
+    
     queue = deque()
-    queue.append((0,0))
-    maps[0][0]=1
-    directions = [(1,0), (-1,0), (0,1), (0,-1)]
-    n = len(maps)
-    m = len(maps[0])
+    queue.append((0,0)) #시작점 넣기
+    
+    dx = [-1, 1,0,0]
+    dy = [0,0,-1,1] #이동키 설정
     
     while queue:
-        row, col = queue.popleft()
-        for dr, dc in directions:
-            new_row = row + dr
-            new_col = col + dc
-            if (0<=new_row<n) and (0<=new_col<m):
-                if maps[new_row][new_col] == 1:
-                    maps[new_row][new_col] = maps[row][col]+1
-                    queue.append((new_row, new_col))
-    if maps[n-1][m-1] > 1:
-        return maps[n-1][m-1]
-    else:
-        return -1
+        x,y = queue.popleft() #현재 위치
+        
+        for i in range(4): #상하좌우 확인
+            nx = x+dx[i]
+            ny = y+dy[i]
+            
+            if 0<=nx<n and 0<=ny<m: #maps 범위 안에 속하는 점인지 확인
+                if maps[nx][ny] ==1:
+                    maps[nx][ny] = maps[x][y]+1
+                    queue.append((nx,ny))
+    if maps[n-1][m-1] == 1:
+            return -1
+        
+    return maps[n-1][m-1]
+            
+        
     
+    
+    return answer
