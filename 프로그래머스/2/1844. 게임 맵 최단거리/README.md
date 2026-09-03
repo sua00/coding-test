8cb2774a0418d8c15fd05d00a3abdd623955fd8c
Sua Kang <88168765+sua00@users.noreply.github.com>
2026-09-03 19:50:49 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 6.43 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
 
 ### 성능 요약
 
-메모리: 11.1 MB, 시간: 6.63 ms
+메모리: 11.3 MB, 시간: 6.43 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 09월 01일 16:14:17
+2026년 09월 03일 19:50:51
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,38 +1,29 @@
-#가중치가 없는 그래프에서 최단거리를 구할 때는 bfs가 대표적
-#bfs는 파이썬에서 queue로 구현
-
 from collections import deque
 
 def solution(maps):
     answer = 0
-    
-    #map 모양 설정
-    m = len(maps[0]) # m : 열(가로 좌표)
-    n = len(maps) #n: 행(세로 좌표)
+    m = len(maps[0])
+    n= len(maps)
     
     queue = deque()
-    queue.append((0,0)) #시작점 넣기
+    queue.append((0,0))
     
-    dx = [-1, 1,0,0]
-    dy = [0,0,-1,1] #이동키 설정
+    dx= [1,-1,0,0]
+    dy =[0,0,1,-1]
     
     while queue:
-        x,y = queue.popleft() #현재 위치
+        x,y= queue.popleft()
         
-        for i in range(4): #상하좌우 확인
+        for i in range(4):
             nx = x+dx[i]
             ny = y+dy[i]
             
-            if 0<=nx<n and 0<=ny<m: #maps 범위 안에 속하는 점인지 확인
-                if maps[nx][ny] ==1:
+            if 0<= nx < n and 0<=ny < m:
+                if maps[nx][ny]==1:
                     maps[nx][ny] = maps[x][y]+1
                     queue.append((nx,ny))
-    if maps[n-1][m-1] == 1:
-            return -1
-        
+    
+    if maps[n-1][m-1] ==1:
+        return -1
+    
     return maps[n-1][m-1]
-            
-        
-    
-    
-    return answer
