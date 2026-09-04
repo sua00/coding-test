05167049003d933b4f8165b04d8e87b5ba45df30
Sua Kang <88168765+sua00@users.noreply.github.com>
2026-09-04 15:32:17 +0900

    [level 3] Title: 여행경로, Time: 0.02 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.py
+++ 프로그래머스/3/43164. 여행경로/여행경로.py
@@ -1,37 +1,29 @@
-#최단거리 신호 없으면 : dfs
-#답이 여러 개 가능하고, 그중 특정 조건(예: 알파벳 순)으로 하나만 골라야 하는가? dfs+백트레킹
+# 한 경로를 끝까지 만들어보고 전체 티켓을 썻는지 확인 -> dfs
 def solution(tickets):
     answer = []
-    routes = {} #딕셔너리 형태로 각 공항에서 갈 수 있는 목적지 저리
-    for ticket in tickets:
-        start, end= ticket[0], ticket[1]
-        if start not in routes:
-            routes[start]= []
-        routes[start].append(end)
-        
-    for airport in routes:
-        routes[airport].sort()
+    tickets.sort() #알파벳 순으로 방문
     
-    n = len(tickets)
-    path=["ICN"]
+    visited = [False]*len(tickets) #티켓 사용 여부가 중요
+    route = ["ICN"] #시작점 고정
     
-    def dfs(current):
-        if len(path) == n+1:
-            return True
-        if current not in routes:
-            return False
+    def dfs(current): #내가 현재 있는 공항
+        if len(route) == len(tickets)+1: #모든 티켓을 다 사용한 경우
+            return True #여행 경로 완성
         
-        for next_airport in routes[current][:]:
-            path.append(next_airport) #path 추가
-            routes[current].remove(next_airport) #티켓 사용
+        for i in range(len(tickets)):
+            start, end = tickets[i]
             
-            if dfs(next_airport):
-                return True
-            
-            #실패하면 백트레킹
-            path.pop()
-            routes[current].append(next_airport)
+            if start == current and not visited[i]: #현재 공항에서 출발하면서 아직 사용하지 않은 티켓
+                visited[i]= True
+                route.append(end)
+                
+                if dfs(end):
+                    return True #이경로로 쭉 갔더니 여행 경로 완성 가능
+                
+                route.pop() #아닌 경우는 이 길로 그만 가기
+                visited[i] = False #티켓 사용도 무르기
         return False
     
     dfs("ICN")
-    return path
+    
+    return route
