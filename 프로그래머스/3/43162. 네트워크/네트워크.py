e0bcab8fbd604a2eefd7d6d8634a8141881410b5
Sua Kang <88168765+sua00@users.noreply.github.com>
2026-09-08 12:59:23 +0900

    [level 3] Title: 네트워크, Time: 0.26 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,16 +1,19 @@
+# 경로 끝까지 가서 모두 연결되어있는 덩어리가 몇 개인지 확인하는 거 -> dfs
 def solution(n, computers):
     answer = 0
-    visited=[False]*n
+    visited = [False]*n
     
-    def dfs(node):
-        visited[node] = True
-        
-        for next_node in range(n):
-            if computers[node][next_node] ==1 and not visited[next_node]:
-                dfs(next_node)
-                
+    def dfs(current):
+        if not visited[current]:
+            visited[current]=True
+            
+            for next_computer in range(n):
+                if computers[current][next_computer] == 1 and not visited[next_computer]:
+                    dfs(next_computer)
+    
     for i in range(n):
         if not visited[i]:
-            answer += 1
             dfs(i)
+            answer+=1
+            
     return answer
