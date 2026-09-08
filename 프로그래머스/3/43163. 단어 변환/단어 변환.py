58b9868efaa559185cb42e2e7d5484d75a008f8e
Sua Kang <88168765+sua00@users.noreply.github.com>
2026-09-08 14:15:45 +0900

    [level 3] Title: 단어 변환, Time: 0.24 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,30 +1,29 @@
+# 가장 짧은 변환 과정 찾기 : bfs
+
 from collections import deque
 
 def solution(begin, target, words):
     answer = 0
     
     queue = deque()
-    visited = [False] * len(words)
-    
     queue.append((begin,0))
+    visited = [False]*len(words)
     
     while queue:
         current, count = queue.popleft()
-        
         if current == target:
             return count
         
         for i in range(len(words)):
-            if visited[i]==True:
-                continue
-            
-            diff = 0
-            for j in range(len(current)) :
-                if current[j] != words[i][j]:
-                    diff +=1
-            
-            if diff ==1:
-                visited[i]= True
-                queue.append((words[i], count+1))
+            if visited[i]==False:
+                diff =0
                 
+                for j in range(len(current)):
+                    if words[i][j] != current[j]:
+                        diff+=1
+                        
+                if diff == 1:
+                    visited[i]=True
+                    queue.append((words[i],count+1))
+    
     return 0
