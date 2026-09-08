178c428ee24b55f95466898ef0585fbdc0a8e527
Sua Kang <88168765+sua00@users.noreply.github.com>
2026-09-08 14:02:25 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 6.45 ms, Memory: 11.1 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,29 +1,33 @@
+#가중치 없는 최단거리는 bfs
+
 from collections import deque
 
 def solution(maps):
     answer = 0
-    m = len(maps[0])
-    n= len(maps)
-    
     queue = deque()
     queue.append((0,0))
     
-    dx= [1,-1,0,0]
-    dy =[0,0,1,-1]
+    n = len(maps)#행
+    m = len(maps[0]) #열
+    
+    #이동키
+    dx = [1,-1,0,0]
+    dy = [0,0,1,-1]
     
     while queue:
-        x,y= queue.popleft()
+        x,y = queue.popleft()
+        
         
         for i in range(4):
             nx = x+dx[i]
             ny = y+dy[i]
             
-            if 0<= nx < n and 0<=ny < m:
+            if 0<=nx<n and 0<=ny<m:
                 if maps[nx][ny]==1:
                     maps[nx][ny] = maps[x][y]+1
+                
                     queue.append((nx,ny))
-    
-    if maps[n-1][m-1] ==1:
+    if maps[n-1][m-1]==1:
         return -1
-    
+                    
     return maps[n-1][m-1]
