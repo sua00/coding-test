15365e8e95b328980d6228d9c104ea1b5ef242fd
Sua Kang <88168765+sua00@users.noreply.github.com>
2026-09-09 15:11:57 +0900

    [level 1] Title: 최소직사각형, Time: 0.76 ms, Memory: 12.9 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
@@ -1,18 +1,17 @@
 def solution(sizes):
     answer = 0
-    temp = 0
-    max_w =0
-    max_h=0
+    max_w, max_h=0,0
     for size in sizes:
-        w, h = size[0], size[1]
-        if w < h:
+        temp = 0
+        w, h = size
+        if w < h :
             temp = h
             h = w
-            w = temp
-            
+            w= temp
         if max_w < w:
             max_w = w
         if max_h < h:
             max_h = h
-    answer = max_w * max_h
+    answer = max_h * max_w
+    
     return answer
