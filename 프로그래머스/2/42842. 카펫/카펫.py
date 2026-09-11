1bbd1cc7c7c7c2b3c0008e07d60ec105c33f6224
Sua Kang <88168765+sua00@users.noreply.github.com>
2026-09-11 13:43:41 +0900

    [level 2] Title: 카펫, Time: 23.48 ms, Memory: 11.2 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -2,9 +2,12 @@ def solution(brown, yellow):
     answer = []
     total = brown + yellow
     
-    for h in range(3, int(total**0.5)+1):
-        if total%h ==0:
-            w= total //h
+    for h in range(3, int(total//2)+1):
+        if total % h ==0:
+            w = total // h
+            
+            if w < h:
+                continue
             
             if (w-2)*(h-2)== yellow:
                 answer = [w,h]
